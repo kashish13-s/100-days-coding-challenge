5f1c2fde41bdfd70d2bc68b5b8a3c375d716a9a1
kashish13-s <ksaklani13.s@gmail.com>
2026-08-16 22:20:20 +0530

    Create Day5_2.c

---
+++ Day5_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int totalSeconds, hours, minutes, seconds;
+    printf("Enter time in seconds: ");
+    scanf("%d", &totalSeconds);
+
+    // Conversion
+    hours = totalSeconds / 3600;          // 1 hour = 3600 seconds
+    minutes = (totalSeconds % 3600) / 60; // remaining minutes
+    seconds = totalSeconds % 60;          // remaining seconds
+
+    // Display result
+    printf("Time = %02d:%02d:%02d\n", hours, minutes, seconds);
+    return 0;
+}
