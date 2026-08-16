5f0b899eba536d4feed006fd17e08aceeaf479b4
kashish13-s <ksaklani13.s@gmail.com>
2026-08-16 22:24:06 +0530

    Update Day6_1.c

--- Day6_1.c
+++ Day6_1.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
-
 int main() {
     int num;
-
-    // Input number
     printf("Enter a number: ");
     scanf("%d", &num);
-
     // Check even or odd
     if (num % 2 == 0) {
         printf("%d is Even\n", num);
     } else {
         printf("%d is Odd\n", num);
     }
-
     return 0;
 }
