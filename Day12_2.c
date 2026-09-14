06148920a1c74d5e4dcf47fbc194bb37efcfa22e
kashish13-s <ksaklani13.s@gmail.com>
2026-09-14 10:46:41 +0530

    Create Day12_2.c

---
+++ Day12_2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int units;
+    int bill = 0;
+
+    // Input units consumed
+    printf("Enter units consumed: ");
+    scanf("%d", &units);
+
+    if (units <= 0) {
+        printf("Invalid input! Units must be positive.\n");
+    }
+    else if (units <= 100) {
+        bill = units * 5;
+    }
+    else if (units <= 200) {
+        bill = (100 * 5) + ((units - 100) * 7);
+    }
+    else if (units <= 300) {
+        bill = (100 * 5) + (100 * 7) + ((units - 200) * 10);
+    }
+    else {
+        bill = (100 * 5) + (100 * 7) + (100 * 10) + ((units - 300) * 12);
+    }
+
+    if (units > 0)
+        printf("Bill: ₹%d\n", bill);
+
+    return 0
