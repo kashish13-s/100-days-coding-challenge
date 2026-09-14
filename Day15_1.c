42755ae7ca94939b50d78844f6f69f577b4336b4
kashish13-s <ksaklani13.s@gmail.com>
2026-09-14 10:54:21 +0530

    Create Day15_1.c

---
+++ Day15_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    long long fact = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("Factorial not defined for negative numbers.\n");
+    } else {
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        printf("Factorial of %d = %lld\n", n, fact);
+    }
+    return 0;
+}
