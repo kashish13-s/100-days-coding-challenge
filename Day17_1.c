3c9a0b12904d66a354a385a9be8b4328498c0fa0
kashish13-s <ksaklani13.s@gmail.com>
2026-09-14 12:04:06 +0530

    Create Day17_1.c

---
+++ Day17_1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int n, sum = 0, digit, temp, digits = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    temp = n;
+    while (temp != 0) {
+        digits++;
+        temp /= 10;
+    }
+
+    temp = n;
+    while (temp != 0) {
+        digit = temp % 10;
+        sum += pow(digit, digits);
+        temp /= 10;
+    }
+
+    if (sum == n)
+        printf("Armstrong Number\n");
+    else
+        printf("Not Armstrong Number\n");
+
+    return 0;
+}
